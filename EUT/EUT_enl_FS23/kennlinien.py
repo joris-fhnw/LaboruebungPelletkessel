5723836ca1f3aa18148aa043a97aa2e5be5fb7bf
joris-fhnw <joris.strassburg@fhnw.ch>
2023-03-15 16:26:41 +0100

    Plots geändert

--- EUT/EUT_enl_FS23/kennlinien.py
+++ EUT/EUT_enl_FS23/kennlinien.py
@@ -2,60 +2,78 @@ from CoolProp.CoolProp import PropsSI
 import numpy as np
 from functions_and_datacontainer import functions as func
 from matplotlib import pyplot as plt
+from functions_and_datacontainer import wood_combustion as wc
 import matplotlib.dates as mdates
 import matplotlib
-matplotlib.use("Qt5Agg") #qtagg
+
+matplotlib.use("Qt5Agg")
 
 # CP Wasser, Wasserdampf und Luft mit Coolprop
-Temp = np.array(range(300,450))  # [K]
-cp_vapor = np.zeros(Temp.size)
-cp_water = np.zeros(Temp.size)
-cp_air = np.zeros(Temp.size)
-density_air = np.zeros(Temp.size)
+dT = 273
+Temp_exhaust = np.array(range((75 + dT), (125+dT)))  # [K] Abgas
+Temp_water = np.array(range(40+dT, 70 + dT))  # [K] Wasserklreislauf
+Temp_comb_air = np.array(range(10+dT, 30 + dT))
+lam = np.array([1.4, 2.3])
+p_amb = 1.01325e5  # [Pa]
 
-p_amb = 1.01325e2  # [hPa]
-for i,T in enumerate(Temp):
-    cp_vapor[i] = PropsSI("C","T",T,"P|gas",p_amb,"Water")   # [J/(kg*K)]
-    cp_water[i] = PropsSI("C", "T|liquid", T, "P", p_amb, "Water")  # [J/(kg*K)]
+
+# Initialisieren der Vektoren
+cp_vapor = np.zeros((lam.size, Temp_exhaust.size))
+cp_air = np.zeros(Temp_exhaust.size)
+cp_exhaust = np.zeros(Temp_exhaust.size)   # Stöchiometrisch!!
+cp_water = np.zeros(Temp_water.size)
+density_water = np.zeros(Temp_water.size)
+density_air = np.zeros(Temp_comb_air.size)
+
+x_h2o = np.zeros(lam.size)
+
+# Berechne Wasseranteil im Abgas für Partialdruck des Dampfes
+pellet = wc.Wood("Pellet", ["C", 0.51, "H", 0.06, "O", 0.43, "N", 0, "S", 0], 0.08)  # Objekt für Rechnung
+for i, l in enumerate(lam): x_h2o[i] = pellet.H2O(l)  # [mol/mol] Molbruch
+p_vapor = x_h2o * p_amb  # [Pa] Partialdruck des Dampfes im Abgas (Auch Abhänig von Lambda)
+
+
+# Berechnung der Dichte und der Wärmekapazitäten
+for i, T in enumerate(Temp_exhaust):
+    # Temperaturbereich hoch (am Ausgang des Kessels)
+    for z in range(lam.size): cp_vapor[z, i] = PropsSI("C", "T", T, "P|gas", p_vapor[z], "Water")  # [J/(kg*K)]
     cp_air[i] = PropsSI("C", "T", T, "P", p_amb, "Air")  # [J/(kg*K)]
-    density_air[i] = PropsSI("D", "T", T, "P", p_amb, "Air")*1e3  # [kg/K]
-
-# CP Abgas ist von Lambda und Temperatur abhängig, wird mit Fitformel berechnet
-lam = np.array([1.4,2.3])
-cp_exhaust = np.zeros((lam.size,Temp.size))
-for z,l in enumerate(lam):
-    for i,T in enumerate(Temp):
-        cp_exhaust[z,i] = func.calc_cp_exhaust(l,T)  # [J/(kg*K)]
-
+    cp_exhaust[i] = func.calc_cp_exhaust(1, T)
+for i, T in enumerate(Temp_water):
+    # Temperaturbereich niedrig (am Eingang des Kessels)
+    cp_water[i] = PropsSI("C", "T|liquid", T, "P", p_amb, "Water")  # [J/(kg*K)]
+    density_water[i] = PropsSI("D", "T|liquid", T, "P", p_amb, "Water")  # [kg/m3]
+for i, T in enumerate(Temp_comb_air):
+    density_air[i] = PropsSI("D", "T", T, "P", p_amb, "Air")  # [kg/m3]
 
 "-----------------------Plot----------------------------------------"
-
+color = ["black", "grey"]  # Falls 2 Plotts in einem Graph
 "CP Water,Vapor and Air"
-fig, axes = plt.subplots(3,1)
-axes[0].plot(Temp-273,cp_vapor,color = "black",label = "cp vapor")
+fig, axes = plt.subplots(3, 1)
+for z in range(lam.size):
+    axes[0].plot(Temp_exhaust - 273, cp_vapor[z], color=color[z],
+                 label=f"cp vapor, lambda: {lam[z]}")
 axes[0].set_ylabel("[J/(kg*K)]")
-# axes[0].set_xlabel("Temperatur [°C]")
 axes[0].legend()
 axes[0].grid()
-axes[1].plot(Temp-273,cp_water,color = "black",label = "cp water")
+
+axes[1].plot(Temp_exhaust - 273, cp_air, color="black", label="cp air")
 axes[1].set_ylabel("[J/(kg*K)]")
-# axes[1].set_xlabel("Temperatur [°C]")
 axes[1].legend()
 axes[1].grid()
-axes[2].plot(Temp-273,cp_air,color = "black",label = "cp air")
+Tmin, Tmax = 10, 80  # [°C] Plot Bereich für Wasser
+axes[2].plot(Temp_water - 273, cp_water, color="black", label="cp water")
+# axes[2].set_xlim([10,80])
+# axes[2].set_ylim([4170,4200])
 axes[2].set_ylabel("[J/(kg*K)]")
 axes[2].set_xlabel("Temperatur [°C]")
 axes[2].legend()
 axes[2].grid()
-# fig.text(0.06, 0.5, 'Wärmekapazität [J/(kg*K)]', ha='center', va='center', rotation='vertical')
 plt.tight_layout()
-# fig.show()
 
 "CP Exhaust"
-color = ["black","grey"]
 fig1 = plt.figure()
-for i,l in enumerate(lam):
-    plt.plot(Temp-273,cp_exhaust[i], color=color[i], label=f"Lambda = {l}")
+plt.plot(Temp_exhaust - 273, cp_exhaust, color="black", label=f"cp exhaust, Lambda = 1")
 plt.ylabel("Wärmekapazität [J/(kg*K]")
 plt.xlabel("Temperatur [°C]")
 plt.grid()
@@ -64,18 +82,33 @@ plt.tight_layout()
 # fig1.show()
 
 "Airdensity"
-fig2 = plt.figure()
-plt.plot(Temp-273,density_air,color = "black",label = f"air density")
-plt.ylabel("Density [kg/m3]")
-plt.xlabel("Temperatur [°C]")
-plt.grid()
-plt.legend()
+fig2, axes2 = plt.subplots(2, 1)
+axes2[0].plot(Temp_comb_air - 273, density_air, color="black", label="air density")
+axes2[0].set_ylabel("[kg/m3]")
+axes2[0].legend()
+axes2[0].grid()
+
+axes2[1].plot(Temp_water - 273, density_water, color="black", label="water density")
+axes2[1].set_ylabel("[kg/m3]")
+axes2[1].legend()
+axes2[1].grid()
+axes2[1].set_xlabel("Temperatur [°C]")
 plt.tight_layout()
-# fig2.show()
+
+# fig2 = plt.figure()
+# plt.plot(Temp-273,density_air,color = "black",label = f"air density")
+# plt.ylabel("Density [kg/m3]")
+# plt.xlabel("Temperatur [°C]")
+# plt.xlim([Tmin,Tmax])
+# plt.ylim([1,1.25])
+# plt.grid()
+# plt.legend()
+# plt.tight_layout()
+# # fig2.show()
 
 plt.show()
 
-save_fig = True
+save_fig = False
 
 if save_fig:
     fig_formate = "png"
@@ -85,4 +118,3 @@ if save_fig:
     fig.savefig(fig_name, format=fig_formate)
     fig1.savefig(fig_name1, format=fig_formate)
     fig2.savefig(fig_name2, format=fig_formate)
-
